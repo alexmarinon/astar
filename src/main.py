8191589b25b99c0a596eaf92119f33ef7adb30b6
alexmarinon <alexmarinon@gmail.com>
2024-04-18 11:24:19 +1000

    Fix

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from classes.Simulation import Simulation
 from tkinter import messagebox
 
-def show_popup():
-    messagebox.showinfo("Important Info on How to Use", f"1. Change Metric switches between Chebyshev & Manhattan heuristic \n2. You can hold down left click to continuously draw obstacles.")
+def show_popup(title: str, message: str):
+    messagebox.showinfo(title, message)
 
 if __name__ == "__main__": # standard, doesnt run if used as part of a module or package
-    show_popup()
+    show_popup("Important Info on How to Use", f"1. Change Metric switches between Chebyshev & Manhattan heuristic \n2. You can hold down left click to continuously draw obstacles.")
     sim = Simulation(50, 50, pass_allow_diagonal=True) # create simulation object with grid 50x50 and using chevyshev distance
     sim.setup((0, 0), (49, 49), [(10, 1)]) # set up with start at 0,0, goal at 49,49, and a random obstacle at 10,1
     sim.run() # run simulation + visualizer
