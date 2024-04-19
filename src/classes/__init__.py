0dc6a492229d947d12d80d92c6f25cf541c82a03
alexmarinon <alexmarinon@gmail.com>
2024-04-19 14:34:13 +1000

    Comment revision

---
+++ src/classes/__init__.py
@@ -0,0 +1,4 @@
+from classes.Simulation import Simulation
+
+
+sim = Simulation(50, 50, pass_allow_diagonal=True) # create simulation object with grid 50x50 and using chevyshev distance
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from classes.Simulation import Simulation
+from classes import sim
 from tkinter import messagebox
 
 """
@@ -15,6 +15,5 @@ def show_popup(title: str, message: str):
 
 if __name__ == "__main__": # standard, doesnt run if used as part of a module or package
     show_popup("Important Info on How to Use", f"1. Change Metric switches between Chebyshev & Manhattan heuristic \n2. You can hold down left click to continuously draw obstacles.")
-    sim = Simulation(50, 50, pass_allow_diagonal=True) # create simulation object with grid 50x50 and using chevyshev distance
     sim.setup((0, 0), (49, 49), [(10, 1)]) # set up with start at 0,0, goal at 49,49, and a random obstacle at 10,1
     sim.run() # run simulation + visualizer
